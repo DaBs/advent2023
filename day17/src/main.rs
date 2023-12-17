6835c592bd60858335fb63566baf4d8f073f9b62
David Alexander Bjerremose <dab@davidbjerremose.com>
2023-12-17 21:35:14 +0100

    tests: added more tests for day17

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -225,4 +225,18 @@ mod tests {
 
         assert_eq!(path, Some(102));
     }
+
+    #[test]
+    fn test_part1() {
+        let input = include_str!("./input.txt");
+
+        assert_eq!(part1(input), 722);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("./input.txt");
+
+        assert_eq!(part2(input), 894);
+    }
 }
